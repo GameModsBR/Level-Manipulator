4aabd4083e79d6bcef7982f5519c675b6da90cba
joserobjr <jose.rob.jr@gmail.com>
2020-01-29 23:57:43 -0300

    Add some mappings (incomplete)

---
+++ catalog/bedrock-edition/src/main/kotlin/br/com/gamemods/levelmanipulator/catalog/bedrock/modern/mapping/ModernBedrockEditionUniversalMapper.kt
@@ -0,0 +1,6 @@
+package br.com.gamemods.levelmanipulator.catalog.bedrock.modern.mapping
+
+internal class ModernBedrockEditionUniversalMapper {
+    var defaultNamespace = "minecraft"
+    var defaultTargetNamespace = "universal-minecraft"
+}
